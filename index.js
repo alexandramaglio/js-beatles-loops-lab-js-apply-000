ba5d648cca015f1196ead421561f9e5b256b522e
Alexandra Middleton <alexandra.middleton@blackrock.com>
2019-03-16 16:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+
+theBeatlesPlay = function(musicians, instruments) {
+  var beatles = [];
+  for(var n=0; n< musicians.length; n++) {
+    beatles[n] = musicians[n] + " plays " + instruments[n];
+  }
+  return beatles;
+};
+
+
+johnLennonFacts = function(factarray) {
+  var n = 0;
+  var excitedfacts = [];
+  while(n < factarray.length) {
+    excitedfacts[n] = factarray[n] + "!!!";
+    n++;
+  }
+  return factarray;
+};
